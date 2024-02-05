8d8502921a84153bf6551f0af56a3d3d265a24cc
Mustafa Önder ÜNLÜ <116373504+mustafaonderunluu@users.noreply.github.com>
2024-02-05 13:55:59 +0300

    Proje dosyası ekle.

---
+++ OverLoading Operators/MyClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverLoading_Operators
+{
+    internal class MyClass
+    {
+        public int X { get; set; }
+        public static MyClass operator +(MyClass left, MyClass right) {
+
+
+            return new MyClass { X = left.X + right.X };
+        }
+    }
+}
---
+++ OverLoading Operators/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverLoading_Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* + - * /
+              == =! < >  <= >=
+             true false  while(true)
+            &&(ve) ||(veya) ?? ? += -= *= /=  */
+
+            MyClass x = new MyClass{X = 10 };
+            MyClass y = new MyClass {X =7 };
+            MyClass z = x + y;  
+            Console.WriteLine(z.X);
+            Console.ReadLine();
+
+        }
+    }
+}
